c04cc23cb355fae0a21a7167d01d66cbdb259911
Jan Machacek <janm@cakesolutions.net>
2013-06-07 08:32:06 +0100

    REST server app

--- jvm/src/main/scala/org/eigengo/sd/rest.scala
+++ jvm/src/main/scala/org/eigengo/sd/rest.scala
@@ -1,6 +1,7 @@
 package org.eigengo.sd
 
-import org.eigengo.sd.core.Core
+import org.eigengo.sd.core.{ConfigCoreConfiguration, Core}
 import org.eigengo.sd.api.Api
 
 
+object Rest extends App with Core with ConfigCoreConfiguration with Api
