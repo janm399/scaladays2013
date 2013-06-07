a6a3b652730506c0ea77f388da985ace6bec9175
Jan Machacek <janm@cakesolutions.net>
2013-06-07 08:14:52 +0100

    Core configuration

--- jvm/src/main/scala/org/eigengo/sd/core/core.scala
+++ jvm/src/main/scala/org/eigengo/sd/core/core.scala
@@ -7,15 +7,34 @@ import com.github.sstone.amqp.ConnectionOwner
 case class Begin(minCoins: Int)
 
 /**
+ * Contains configuration for the core
+ */ 
+trait CoreConfiguration {
+
+  def amqpConnectionFactory: ConnectionFactory
+
+}
+
+/**
+ * Implementation of the ``CoreConfiguration`` that uses the underlying ``system``'s ``settings``.
+ */
+trait ConfigCoreConfiguration extends CoreConfiguration {
+  def system: ActorSystem
+
+  private val amqpHost = system.settings.config.getString("spray-akka.amqp.host")
+  // connection factory
+  val amqpConnectionFactory = new ConnectionFactory(); amqpConnectionFactory.setHost(amqpHost)
+
+}
+
+/**
  * Contains the functionality of the "headless" part of our app
  */
 trait Core {
+  this: CoreConfiguration =>
 
   // start the actor system
   implicit val system = ActorSystem("recog")
-
-  val amqpConnectionFactory = new ConnectionFactory
-  amqpConnectionFactory.setHost("localhost")
 
   // create a "connection owner" actor, which will try and reconnect automatically if the connection ins lost
   val amqpConnection = system.actorOf(Props(new ConnectionOwner(amqpConnectionFactory)))
